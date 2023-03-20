f66c8f6cd3d21a032a737ee0b3a79c2a8e32fe22
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-20 02:37:38 -0700

    explr/feat: reads input, recursively prints file, writes to input dir

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ static ASSETS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/assets");
 #[command(author, version, about, long_about = None)]
 struct SomeStruct {
     #[command(subcommand)]
-    subcommand: SubCommand,
+    subcommand_field: SubCommand,
     arg: String,
 }
 
@@ -23,23 +23,50 @@ enum SubCommand {
 }
 
 fn main() {
-    let git = SomeStruct::interactive_parse().unwrap();
-    println!("{:?}", git);
+    let user_input = SomeStruct::interactive_parse().unwrap();
+    println!("{:?}", user_input);
 
-    // // of course, you can retrieve a file by its full path
-    // let asst_tree = PROJECT_DIR.get_file("assets/").unwrap();
+    /////////////////////////////////
 
-    // // copy PROJECT_DIR to a current directory
-    // ASSETS_DIR.extract("created-assets/").unwrap();
-    // let entries = ASSETS_DIR.entries();
-    let dirs = ASSETS_DIR.dirs();
+    let assets = ASSETS_DIR.clone();
 
-    for entry in dirs {
-        println!("{:?}", entry);
+    let entries = ASSETS_DIR.entries();
+
+    println!("-------------");
+    for entry in entries {
+        println!("{:?}\n\n", entry.children());
+        match entry {
+            include_dir::DirEntry::File(file) => {
+                println!("FILE: {:?}\n\n", file);
+            }
+            include_dir::DirEntry::Dir(dir) => {
+                println!("DIR: {:?}\n\n", dir);
+            }
+        }
     }
-    // let equality = entries == dirs;
 
-    // // This only lists files shallowly, not recursively
-    // let files = ASSETS_DIR.files().collect::<Vec<_>>();
-    // dbg!(files);
+    println!("-------------");
+    recursive_print(assets);
+
+    println!("-------------");
+    println!("-------------");
+    println!("-------------");
+    println!("Writing files to {:?}", user_input.arg);
+    // copy PROJECT_DIR to a current directory
+    let path = format!("{}/", user_input.arg);
+    ASSETS_DIR.extract(path).unwrap();
+}
+
+fn recursive_print(dir: Dir) {
+    for entry in dir.entries() {
+        match entry {
+            include_dir::DirEntry::File(file) => {
+                println!("FILE: {:?}\n\n", file);
+            }
+            include_dir::DirEntry::Dir(dir) => {
+                println!("DIR: {:?}\n\n", dir);
+                recursive_print(dir.clone());
+            }
+        }
+    }
 }
