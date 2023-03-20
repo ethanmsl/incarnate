55f7b7a608f26cd45078f193257a53c93388d412
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-20 07:00:21 -0700

    refact: separated logic (recursive func) to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use include_dir::Dir;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn recursive_replace(dir: Dir, pattern_val_pairs: &[(&str, &String)]) {
+    for entry in dir.entries() {
+        match entry {
+            include_dir::DirEntry::File(file) => {
+                let file_raw = file
+                    .contents_utf8()
+                    .expect("failure at existance of `contents_utf8`")
+                    .to_string();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+                let file_h = pattern_val_pairs
+                    .iter()
+                    .fold(file_raw, |acc, pair| acc.replace(pair.0, pair.1));
+
+                println!("Writing file to {:?}", file.path());
+                std::fs::create_dir_all(file.path().parent().expect("no parent"))
+                    .expect("unable to create dir");
+                std::fs::write(file.path(), file_h).expect("unable to write file");
+            }
+            include_dir::DirEntry::Dir(dir) => {
+                recursive_replace(dir.clone(), pattern_val_pairs);
+            }
+        }
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -60,30 +60,5 @@ fn main() {
     let newb = Dir::new(&path, ASSETS_DIR.entries());
     println!("newb: {:?}", newb);
 
-    recursive_replace(newb, &replacement_pairs);
-}
-
-fn recursive_replace(dir: Dir, pattern_val_pairs: &[(&str, &String)]) {
-    for entry in dir.entries() {
-        match entry {
-            include_dir::DirEntry::File(file) => {
-                let file_raw = file
-                    .contents_utf8()
-                    .expect("failure at existance of `contents_utf8`")
-                    .to_string();
-
-                let file_h = pattern_val_pairs
-                    .iter()
-                    .fold(file_raw, |acc, pair| acc.replace(pair.0, pair.1));
-
-                println!("Writing file to {:?}", file.path());
-                std::fs::create_dir_all(file.path().parent().expect("no parent"))
-                    .expect("unable to create dir");
-                std::fs::write(file.path(), file_h).expect("unable to write file");
-            }
-            include_dir::DirEntry::Dir(dir) => {
-                recursive_replace(dir.clone(), pattern_val_pairs);
-            }
-        }
-    }
+    incarnate::recursive_replace(newb, &replacement_pairs);
 }
