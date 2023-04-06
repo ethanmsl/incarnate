0d6e584df90f2834158d06a7e3c3e57a65d0a398
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-04-06 11:56:41 -0700

    build: separating out template into its own repo/submodule

--- assets/${{ carnate.python-skeleton }}/src/${{ carnate.project_name }}/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This will be the landing page for the auto-generated GitHub pages site.
-The line below will pull in the `README.md` file from the root of the repo.
-
-
-.. include:: ../../README.md
-"""
--- assets/${{ carnate.python-skeleton }}/tests/test__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-May not be anything to test here as `__main__.py` is typically just an entry point
-into the program
-"""
-
-
-def test_truisim():
-    """Test: True is True"""
-    assert True
