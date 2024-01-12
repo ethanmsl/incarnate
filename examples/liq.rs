062433a8bc050a95706ef67796de6981a0908c42
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-01-12 04:41:15 +0100

    explore: two examples files testing new dependencies' functionality

---
+++ examples/liq.rs
@@ -0,0 +1,21 @@
+//! # Dev Examples - example `liquid` code
+//! Note: These examples are here for dev use and functionality testing.
+//!
+//! Currently looking at `liquid` to replace current custom pattern replace.
+//! (Use of existing piping system sounds particularly useful.)
+
+fn main() -> anyhow::Result<()> {
+        let globals = liquid::object!({
+            "num": 78_f64
+        });
+
+        let template = liquid::ParserBuilder::with_stdlib()
+                .build()?
+                .parse("Liquid! {{num | minus: 12}}")
+                .unwrap();
+
+        let output = template.render(&globals).unwrap();
+        println!("rendered template: {}", output);
+
+        Ok(())
+}
---
+++ examples/walk.rs
@@ -0,0 +1,14 @@
+//! # Dev Examples - example `walkdir` code
+//! Note: These examples are here for dev use and functionality testing.
+//!
+//! Currently looking at `walkdir` to replace recursive walker that the original code used.
+
+use walkdir::WalkDir;
+
+fn main() -> anyhow::Result<()> {
+        for entry in WalkDir::new("assets") {
+                let entry = entry?;
+                println!("{}", entry.path().display());
+        }
+        Ok(())
+}
